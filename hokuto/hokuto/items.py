bd334ffbc047ee90c47a231a9df8800f06938948
jackdbd <jackdebidda@gmail.com>
2018-08-06 10:15:13 +0200

    Add scrapy spider for characters

--- hokuto/hokuto/items.py
+++ hokuto/hokuto/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class HokutoItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class HokutoNoKenCharacter(scrapy.Item):
+    name = scrapy.Field()
+    url = scrapy.Field()
+    not_in_manga = scrapy.Field()
---
+++ hokuto/hokuto/spiders/characters_spider.py
@@ -0,0 +1,32 @@
+import scrapy
+from ..items import HokutoNoKenCharacter
+
+
+class CharactersSpider(scrapy.Spider):
+    name = "characters"
+    allowed_domains = ["http://hokuto.wikia.com"]
+    start_urls = ["http://hokuto.wikia.com/wiki/List_of_Hokuto_no_Ken_characters"]
+
+    def parse(self, response):
+        root = scrapy.Selector(response)
+
+        li = root.xpath('//div[@id="mw-content-text"]/ul/li')
+        # Kenshiro: <b><a></a></b>
+        # Characters not in manga: <i><a></a></i>
+        # Everyone else: <a></a>
+        for (is_not_in_manga, xpath_query) in (
+            (False, "b/a"), (True, "i/a"), (False, "a")
+        ):
+            characters = li.xpath((xpath_query))
+            for character in characters:
+                name = character.xpath("text()").extract()[0]
+                not_in_manga = True if is_not_in_manga else False
+                if "(page does not exist)" in character.attrib["title"]:
+                    url = None
+                else:
+                    url = character.xpath("@href").extract()[0]
+
+                item = HokutoNoKenCharacter(
+                    name=name, not_in_manga=not_in_manga, url=url
+                )
+                yield item
