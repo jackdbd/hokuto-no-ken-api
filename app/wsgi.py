8ec45aaf68b9505df80bacd04de94686e6c7bbf4
jackdbd <jackdebidda@gmail.com>
2018-08-11 17:14:22 +0200

    Add Procfile for Heroku

--- app/wsgi.py
+++ app/wsgi.py
@@ -2,7 +2,7 @@
 
 Since this Flask application uses the app factory pattern, this python module
 can be used to instantiate the Flask app. This is useful when starting the WSGI
-application with Gunicorn.
+application with Gunicorn (e.g. in a Procfile or in a Dockerfile).
 
 Usage:
     gunicorn --bind 0.0.0.0:$PORT --access-logfile - "app.wsgi:application"
